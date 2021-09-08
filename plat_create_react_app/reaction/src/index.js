8f7241dd218577072ec7b9d90a3e73114ba47aa6
jsprofessional <jshuman@protonmail.com>
2021-09-08 15:00:33 -0500

    Finished first_jsx. Broke plat_create_react_app

--- plat_create_react_app/reaction/src/index.js
+++ plat_create_react_app/reaction/src/index.js
@@ -5,9 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
+    <h1>YEET</h1>,
   document.getElementById('root')
 );
 
